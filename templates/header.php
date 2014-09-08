31ffe89c09bef422acc6126a2e0f9b77c3ed8e32
Ben Everard <hello@beneverard.co.uk>
2014-09-08 22:49:47 +0100

    Added header and footer template files

---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<meta name="description" content="Add a description">
+		<meta name="author" content="Author Name">
+		<meta name="copyright" content="Year, Site Title" />
+
+		<!-- If it is so -->
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="cleartype" content="on">
+
+		<!-- STYLEGUIDE ASSETS -->
+		<link rel="stylesheet" type="text/css" href="/assets/css/screen.min.css" />
+
+	</head>
+
+	<body>
