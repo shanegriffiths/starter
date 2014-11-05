e58a96006c6618631c78832a3396cb28ff82f5ad
Connor Holyday <connorholyday@gmail.com>
2014-11-05 13:35:01 +0000

    Implemented demo styling

--- _templates/atoms/layout/grid.php
+++ _templates/atoms/layout/grid.php
@@ -1,40 +1,40 @@
 <p>Demo of how the grid classes work</p>
 
 <div class="row">
-	<div class="col S-all / demo__block demo__grid">
+	<div class="col S-all / demo__grid">
 		<span>S-all</span>
 	</div>
 </div>
 
 <div class="row">
 
-	<div class="col S-all M-3 / demo__block demo__grid">
+	<div class="col S-all M-3 / demo__grid">
 		<span>S-all M-3</span>
 	</div>
 
-	<div class="col S-all M-3 / demo__block demo__grid">
+	<div class="col S-all M-3 / demo__grid">
 		<span>S-all M-3</span>
 	</div>
 </div>
 
 <div class="row">
 
-	<div class="col S-hide L-6 / demo__block demo__grid">
+	<div class="col S-hide L-6 / demo__grid">
 		<span>S-hide L-6</span>
 	</div>
 
-	<div class="col S-all L-6 / demo__block demo__grid">
+	<div class="col S-all L-6 / demo__grid">
 		<span>S-all L-6</span>
 	</div>
 </div>
 
 <div class="row">
 
-	<div class="col S-all M-3 L-hide / demo__block demo__grid">
+	<div class="col S-all M-3 L-hide / demo__grid">
 		<span>S-all M-3 L-hide</span>
 	</div>
 
-	<div class="col S-all M-3 L-hide / demo__block demo__grid">
+	<div class="col S-all M-3 L-hide / demo__grid">
 		<span>S-all M-3 L-hide</span>
 	</div>
 
