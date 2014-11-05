b1df815d85d160226a0e383ce36a1bbb893a8e51
Connor Holyday <connorholyday@gmail.com>
2014-11-05 13:45:07 +0000

    Fixed typo on php include

--- _templates/organisms/global/header.php
+++ _templates/organisms/global/header.php
@@ -1,6 +1,6 @@
 <!-- Begin .header -->
 <header class="header cf">
 
-	<?php sg_include(['molecules', 'navigation', 'top-nav']); ?>
+	<?php sg_include(['molecules', 'navigation', 'secondary-nav']); ?>
 
 </header>
