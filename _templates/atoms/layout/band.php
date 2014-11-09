855b2512eb8443245f8070a439697c2a2e231db7
Shane Griffiths <shane@theideabureau.co>
2014-11-09 11:07:47 +0000

    Updated band content

--- _templates/atoms/layout/band.php
+++ _templates/atoms/layout/band.php
@@ -1,12 +1,11 @@
 <span class="sg-note">The band classes are used on a section to add top and bottom MARGIN to a content section. This is to ensure vertical rhythm is upheld as best as possible.</span>
 
+<div class="demo__block band">
+	<p class="text--micro"><code>.band</code> is the value of $space (the same as the line-height of the paragraph) = 1.5em (27px)</p>
+</div>
 
 <div class="demo__block band band--thin">
 	<p class="text--micro"><code>.band--thin</code> is half of $space</p>
-</div>
-
-<div class="demo__block band">
-	<p class="text--micro"><code>.band</code> is the value of $space (the same as the line-height of the paragraph) = 1.5em (27px)</p>
 </div>
 
 <div class="demo__block band band--thick">
@@ -17,6 +16,6 @@
 	<p class="text--micro"><code>.band--extra-thick</code> is 3 times the size as $space</p>
 </div>
 
-<div class="demo__block band band--thick">
-	<p class="text--micro"><code>.band--double-thick</code> is twice the size as .band--thick</p>
+<div class="demo__block band band--double-thick">
+	<p class="text--micro"><code>.band--double-thick</code> is 4 times the size as $space</p>
 </div>
