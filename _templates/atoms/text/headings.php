3e4efb28d2b442f82f06c30deba8f9a833636b80
Shane Griffiths <shane@theideabureau.co>
2014-11-09 11:15:14 +0000

    Added more generic headings

--- _templates/atoms/text/headings.php
+++ _templates/atoms/text/headings.php
@@ -1,25 +1,20 @@
-<span class="sg-note">Heading 1 - 50px / 41px (1.2)</span>
-<h1>the quick brown fox jumps over the lazy dog</h1>
+<span class="sg-note">Heading 1 - 40px / 48px (1.2)</span>
+<h1>Heading 1</h1>
 
-<span class="sg-note">Heading 2 - 35px / 42px (1.2)</span>
-<h2>the quick brown fox jumps over the lazy dog</h2>
+<span class="sg-note">Heading 2 - 30px / 36px (1.2)</span>
+<h2>Heading 2</h2>
 
-<span class="sg-note">Heading 3 - 30px / 36px (1.2)</span>
-<h3>the quick brown fox jumps over the lazy dog</h3>
+<span class="sg-note">Heading 3 - 20px / 24px (1.2)</span>
+<h3>Heading 3</h3>
 
-<span class="sg-note">Heading 4 - 20px / 24px</span>
-<h4>the quick brown fox jumps over the lazy dog</h4>
+<span class="sg-note">Heading 4 - 18px / 21px</span>
+<h4>Heading 4</h4>
 
-<span class="sg-note">Heading 5 - 18px / 22px</span>
-<h5>the quick brown fox jumps over the lazy dog</h5>
+<span class="sg-note">Heading 5 - 16px / 19px</span>
+<h5>Heading 5</h5>
 
 <hr />
 
 <span class="sg-note">With <code>.has-strap</code> class added. Only add this class when you'd like to add a strapline under the main title. The strapline underneath is a standard <code>h2</code> with <code>.strap</code> applied</span>
-<h1 class="has-strap">the quick brown fox jumps over the lazy dog</h1>
-<h2 class="strap">the quick brown fox jumps over the lazy dog</h2>
-
-<h2 class="has-strap">the quick brown fox jumps over the lazy dog</h2>
-<h3 class="strap">the quick brown fox jumps over the lazy dog</h3>
-
-<span class="sg-note">Items using the <code>.has-strap</code> class will inherit a margin-bottom which is 75% smaller than the standard margin-bottom. i.e. h2 will have a margin bottom of 0.3em instead of 1.2em</span>
+<h1 class="has-strap">Client Portfolio</h1>
+<h2 class="strap">We’re lucky to have a fantastic network of clients who share our vision.</h2>
