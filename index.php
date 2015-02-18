c6306851d73890a825402ff048d6c263f18cbd77
Ben Everard <hello@beneverard.co.uk>
2015-02-18 12:53:12 +0000

    Removed header and footer partials
    For this basic front-end boilerplate (much like H5BP) we only really need a single file... this can be split out into partials in the style guide or in the platform.

--- index.php
+++ index.php
@@ -1,13 +1,113 @@
-<?php include('header.php'); ?>
+<!DOCTYPE html>
+<html class="no-js">
+	
+	<head>
 
-    <main>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-        <div class="wrapper">
+		<meta name="description" content="Add a description">
+		<meta name="author" content="Author Name">
+		<meta name="copyright" content="Year, Site Title" />
 
-            <a href="/styleguide" class="btn">Access the Styleguide</a>
+		<!-- If it is so -->
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
 
-        </div>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="cleartype" content="on">
 
-    </main>
+		<!--
+		TODO: Set Android Lollipop colour on Chrome
+		-->
 
-<?php include('footer.php'); ?>
+		<meta name="theme-color" content="#db5945">
+
+		<!--
+		TODO: Set MS Windows 8 tile icon and colour
+		-->
+
+		<meta name="msapplication-TileImage" content="">
+		<meta name="msapplication-TileColor" content="#222222">
+
+		<title>Add a title</title>
+
+		<!-- For iPhone 4 -->
+		<!-- <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/img/favicons/apple-touch-icon.png"> -->
+		<!-- For iPad 1-->
+		<!-- <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/img/favicons/apple-touch-icon.png">-->
+		<!-- For the new iPad -->
+		<!-- <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/img/favicons/apple-touch-icon-144x144-precomposed.png">-->
+		<!-- For iPhone 3G, iPod Touch and Android -->
+		<!-- <link rel="apple-touch-icon-precomposed" href="/assets/img/favicons/apple-touch-icon-precomposed.png">-->
+		<!-- For Nokia -->
+		<!-- <link rel="shortcut icon" href="/assets/img/favicons/apple-touch-icon.png">-->
+		<!-- For everything else -->
+		<!-- <link rel="shortcut icon" href="/assets/img/favicons/favicon.ico">-->
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+		<link rel="stylesheet" href="/assets/css/styles.css" />
+
+		<script src="assets/js/libs/modernizr.js"></script>
+
+	</head>
+
+	<body>
+
+	    <header class="site-header">
+
+			<div class="wrapper">
+
+				<h1>The Boilerplate</h1>
+
+			    <!-- <nav class="site-nav" role="navigation">
+
+					<ul>
+
+						<li><a href="#" title="#">Home</a></li>
+
+						<li><a href="#" title="#">About Us</a></li>
+
+						<li><a href="#" title="#">Our Work</a></li>
+
+						<li><a href="#" title="#">Get In Touch</a></li>
+
+						<li><a href="#" title="#">Blog</a></li>
+
+					</ul>
+
+				</nav>
+
+			</div> -->
+			<!-- / .wrapper -->
+
+		</header> <!-- / .site-header -->
+
+	    <main>
+
+	        <div class="wrapper">
+
+	            <a href="/styleguide" class="btn">Access the Styleguide</a>
+
+	        </div>
+
+	    </main>
+
+		<!-- <footer class="site-footer">
+
+			<div class="wrapper">
+				<p>Made by The Idea Bureau</p>
+			</div>
+
+		</footer> -->
+		<!-- / .site-footer -->
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+		<script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.11.1.min.js"><\/script>')</script>
+
+		<script src="assets/js/main.js"></script>
+
+	</body>
+
+</html>
