cbe4a248f7f633dd5bb0dd83d2d189e592183c41
Connor Holyday <connorholyday@gmail.com>
2015-03-02 10:20:45 +0000

    Added a JavaScript SVG Fallback

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
 
-    //Enter your JS
+    //*********************
+    // SVG ERROR REPORTING
+    //
+    // <img src="example.svg" data-fallback="example.png">
 
-});
+    if ( ! Modernizr.svg ) {
+
+        $('img[data-fallback]').each(function() {
+
+            // cache image element
+            var $img = $(this);
+
+            // replace svg with fallback
+            $img.attr('src', $img.data('fallback'));
+
+        });
+
+    }
+
 
