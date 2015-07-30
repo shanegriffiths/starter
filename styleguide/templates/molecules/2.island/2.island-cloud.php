dc0e09c3515356b76077a2657b37b8d0cf2a5725
Shane Griffiths <shane@theideabureau.co>
2015-07-30 18:28:49 +0100

    Removed island cloud

--- styleguide/templates/molecules/2.island/2.island-cloud.php
+++
@@ -1,6 +0,0 @@
-<div class="island island--cloud">
-
-	<h3>Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</h3>
-	<p>Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
-
-</div>
