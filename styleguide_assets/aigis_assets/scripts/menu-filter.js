70db094586b57e6ed41c8f68aeb6b55e06330d62
Connor Holyday <connorholyday@gmail.com>
2017-02-15 16:59:54 +0000

    Implemented styleguide search

---
+++ styleguide_assets/aigis_assets/scripts/menu-filter.js
@@ -0,0 +1,84 @@
+export default class MenuFilter {
+
+	// pass in the table
+	constructor(menu, search_input) {
+
+		// set defaults
+		// all nodes are selected by looking under the passed in table node
+		// this prevents cross-contamination
+		this.filter = '';
+		this.menu = menu;
+		this.dropdowns = menu.querySelectorAll('li[data-path-depth="0"]');
+		this.menu_items = menu.querySelectorAll('[data-path-depth="1"] a');
+
+		// set event listeners
+		this.setSearch(search_input);
+
+	}
+
+	setSearch(search_input) {
+
+		// add a click event listener to each
+		search_input.addEventListener('input', () => {
+
+			// update filter
+			this.filter = search_input.value;
+
+			// then refresh the view
+			this.refreshFilter();
+
+		}, false);
+
+	}
+
+	refreshFilter() {
+
+		// if there is a filter set
+		if ( this.filter.length ) {
+
+			this.menu.classList.add('search-active');
+
+			// loop through each table row
+			for ( let item of this.menu_items ) {
+
+				// hide the row
+				item.parentNode.style.display = 'none';
+
+				// if the item matches the filter
+				// display the item
+				if ( item.textContent.indexOf(this.filter) !== -1) {
+					item.parentNode.style.display = 'block';
+				}
+
+			}
+
+		}
+		// otherwise reset all of the items
+		else {
+
+			this.menu.classList.remove('search-active');
+
+			// loop through each table row
+			for ( let item of this.menu_items ) {
+				item.parentNode.style.display = 'block';
+			}
+
+		}
+
+	}
+
+	clearFilter() {
+
+		// reset the filter
+		this.filter = '';
+
+		this.menu.classList.remove('search-active');
+
+		// reset each table row
+		for ( let item of this.menu_items ) {
+			item.parentNode.style.display = 'block';
+		}
+
+	}
+
+}
